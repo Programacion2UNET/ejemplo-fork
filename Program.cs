2a021505c0f3e9eb2fe791e43ecc80647765b6c7
overmachine <frascavictor@gmail.com>
2016-08-04 11:22:42 -0400

    modificando mi nombre 2

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EjemploFork
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Programación II (Sección I)");
+			Console.WriteLine ("==");
+			Console.WriteLine ("\n\nProf. Miguel Useche");
+            Console.WriteLine("Luis Torres");
+
+
+
+            Console.WriteLine("Frasca victor");
+
+		}
+	}
+}
+
